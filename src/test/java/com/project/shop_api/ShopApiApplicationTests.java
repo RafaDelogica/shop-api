220f7baa3049f22610869ba643bb39a55af9c180
Rafael Manuel Sanchez Villanueva <32065917F@oficina.delogica.com>
2026-03-24 19:38:03 +0100

    Initial commit - shop-api

---
+++ src/test/java/com/project/shop_api/ShopApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.project.shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+class ShopApiApplicationTests {
+
+	/*
+	 * @Test void contextLoads() { }
+	 */
+
+}
