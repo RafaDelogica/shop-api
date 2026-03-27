bf9d046ecef097fe810dce0a9b38b95defc9fffa
RafaDelogica <rafael.sanchez@delogica.com>
2026-03-27 17:25:27 +0100

    ProductController added

--- src/main/java/com/project/shop_api/infrastructure/rest/controller/ProductController.java
+++ src/main/java/com/project/shop_api/infrastructure/rest/controller/ProductController.java
@@ -1,5 +1,63 @@
 package com.project.shop_api.infrastructure.rest.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.shop_api.application.service.ProductService;
+import com.project.shop_api.infrastructure.mapper.dto.ProductDtoMapper;
+import com.project.shop_api.infrastructure.rest.dto.request.ProductRequest;
+import com.project.shop_api.infrastructure.rest.dto.response.ProductResponse;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductService service;
+    private final ProductDtoMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest request) {
+        return ResponseEntity.ok(mapper.toResponse(service.create(mapper.toDomain(request))));
+    }
+
+    @GetMapping
+    public Page<ProductResponse> getFilterProduct(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Boolean active,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size,
+            @RequestParam(defaultValue = "name,asc") String sort) {
+
+        return service.findAll(name, active, page, size, sort).map(mapper::toResponse);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable Long id) {
+        return ResponseEntity.ok(mapper.toResponse(service.findById(id)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductRequest request) {
+
+        return ResponseEntity.ok(mapper.toResponse(service.update(id, mapper.toDomain(request))));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
