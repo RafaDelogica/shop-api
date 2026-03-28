383a355851662d90e90c288e7068d08833d7f2e2
RafaDelogica <rafael.sanchez@delogica.com>
2026-03-28 18:27:44 +0100

    Customized exception added to ProductServiceImpl

--- src/main/java/com/project/shop_api/application/service/impl/ProductServiceImpl.java
+++ src/main/java/com/project/shop_api/application/service/impl/ProductServiceImpl.java
@@ -4,6 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.project.shop_api.application.common.exception.BadRequestException;
+import com.project.shop_api.application.common.exception.ConflictException;
+import com.project.shop_api.application.common.exception.ValidationException;
+import com.project.shop_api.application.common.exception.ResourceNotFoundException;
 import com.project.shop_api.application.service.ProductService;
 import com.project.shop_api.domain.model.Product;
 import com.project.shop_api.domain.repository.ProductRepository;
@@ -26,7 +30,12 @@ public class ProductServiceImpl implements ProductService{
 		
 		if(repository.existsBySku(product.getSku())) {
 			log.warn("Product with SKU duplicated", product.getSku());
-			throw new RuntimeException("Ya existe un producto con el SKU " + product.getSku());
+			throw new ConflictException("The SKU " + product.getSku() + " already exists");
+		}
+		
+		if(product.getPrice().doubleValue() <= 0) {
+			log.warn("Product with price lower than 0", product.getSku());
+			throw new ValidationException("The price must be greater than 0");
 		}
 		
 		Product productCreated = repository.save(product);
@@ -45,13 +54,18 @@ public class ProductServiceImpl implements ProductService{
 		
 		if(!existingProduct.getSku().equals(product.getSku()) && repository.existsBySku(product.getSku())){
 			log.warn("Product with SKU duplicated", product.getSku());
-			throw new RuntimeException("Ya existe un producto con el SKU " + product.getSku());
+			throw new ConflictException("The SKU " + product.getSku() + " already exists");
+		}
+		
+		if(product.getPrice().doubleValue() <= 0) {
+			log.warn("Product with price lower than 0", product.getSku());
+			throw new ValidationException("The price must be greater than 0");
 		}
 		
 		product.setId(id);
 		Product productUpdated = repository.save(product);
 		
-		log.info("Product updated with ID: {}", id);
+		log.info("Product updated with ID: {}", productUpdated.getId());
 		
 		return productUpdated;
 	}
@@ -66,7 +80,7 @@ public class ProductServiceImpl implements ProductService{
 		return repository.findById(id)
                 .orElseThrow(() -> {
                     log.warn("Product not found with ID: {}", id);
-                    return new RuntimeException("Producto no encontrado: " + id);
+                    return new ResourceNotFoundException("Producto no encontrado con ID " + id);
                 });
 
 	}
@@ -77,8 +91,20 @@ public class ProductServiceImpl implements ProductService{
 		
 		log.info("Product list with filters name='{}', active='{}'", name, active);
 		
+
+		if (page < 0) {
+			log.warn("Page param must be greater than 0, page='{}'", page);
+			throw new BadRequestException("El parámetro 'page' debe ser >= 0");
+		}
+
+		if (size <= 0) {
+			log.warn("Size param must be greater than 0, size='{}'", size);
+			throw new BadRequestException("El parámetro 'size' debe ser > 0");
+		}
+
+		
 		return repository.findAllPaged(name, active, page, size, sort);
-	}
+		}
 
 	@Override
 	public void delete(Long id) {
@@ -86,6 +112,11 @@ public class ProductServiceImpl implements ProductService{
 		log.info("Logical product deletion with ID: {}", id);
 		
 		Product product = findById(id);
+
+		if (!product.isActive()) {
+            throw new BadRequestException("El producto ya estaba inactivo.");
+        }
+		
 		product.setActive(false);
 		repository.save(product);
 		
